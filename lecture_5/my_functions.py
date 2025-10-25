b0f50b88139164fe3e3baabf3f756de1456a99f4
varvara murashova <vmurashova2@gmail.com>
2025-10-25 16:04:06 +0300

    Update my_functions.py

--- lecture_5/my_functions.py
+++ lecture_5/my_functions.py
@@ -3,20 +3,21 @@
 # change_name - принимает строку, а возвращает измененное имя, где каждая вторая буква - большая
 # greet - принимает строку, ничего не возвращает, а просто принтит "Hello имя" с помощью украшений из rich print
 
-name = input('What is your name?:')
+import rich
+from rich import print
 
-def changed_name(name: str) -> str:
+def change_name(name: str) -> str:
     letters = list(name.lower())
     for i in range(1, len(letters), 2): #(start, stop, step)
         letters[i] = letters[i].upper()
     return ''.join(letters)
 
-new_name = changed_name(name)
+def greet(name: str) -> None:
+    print(f"[bold cyan]Hello[/bold cyan] [bold magenta]{name}[/bold magenta]!")
 
-from rich import print 
-def greet(new_name):
-    print(f"Hello [bold blue]{new_name}[/bold blue]!")
-greet(new_name)
+name = input('What is your name?: ')
+changed_name = change_name(name)
+greet(changed_name)
 
 
 
